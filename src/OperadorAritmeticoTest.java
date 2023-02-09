91356bfcb09faca898fc510b6924b416c722c496
Sara <106694716+SaraLi9789@gmail.com.github.com>
2023-02-09 18:01:47 +0100

    Segunda parte

---
+++ src/OperadorAritmeticoTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class OperadorAritmeticoTest {
+
+	@Test
+	void testSuma() {
+		fail("Not yet implemented");
+	}
+
+}
